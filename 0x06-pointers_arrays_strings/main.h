d35933dc7107aefc7331a41c3e7ed8bdffcbb661
DEV-Shaozi <shalomfaithfulness01@gmail.com>
2023-08-27 16:10:29 -0700

    infinie add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,6 @@ int _putchar(char c);
 void print_number(int n);
 char *rot13(char *s);
 int main(void);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
