1a051e7918495718d4e8cb2e48410f60cfd48fcd
DEV-Shaozi <shalomfaithfulness01@gmail.com>
2024-01-15 16:44:35 -0800

    create c from python

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - modules of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: module
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+	
+}
